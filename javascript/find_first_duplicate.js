42ba5a08971c17a2c57350d3f7ddf2544ddabdfc
Ian Bachiller <ianbachise@gmail.com>
2024-04-11 16:05:51 -0400

    completed day 2 ds and algo: find first duplicate

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,6 +1,27 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  const dupl = [];
+  const retDupl = [];
+  for (i = 0; i < arr.length; i++){
+    if(dupl.includes(arr[i])){ 
+      retDupl.push(arr[i])
+    } else { 
+      dupl.push(arr[i])
+    };
+  }
+  if(retDupl.length > 0) {
+    return retDupl[0]
+  }else{
+    return -1
+  } 
 }
+//create two empty arrays
+// for every element of arr, check if that element is in dupl array, if it is, add it to retDupl array. if not, add it to dupl array. 
+//if retDupl array is not empty, return the element in that array. 
+//otherwise, return -1
+
+//explanation:
+//make two empty arrays to push/add non duplicate or duplicate number to the appropriate array. iterate over each element and check if that element is in the dupl array; if it is, add it to the retDupl array (where duplicate numbers are stored). After finishing looping, check if retDupl is not empty. if it is not, return the value/element in that array. otherwise, return -1. 
+
 
 if (require.main === module) {
   // add your own tests in here
